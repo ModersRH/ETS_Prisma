87b03be8154caa1bb6506638d867416bb65b81b2
Modesto <Modesto@DESKTOP-1VCCBLT>
2022-04-23 19:08:15 +0100

    Commit Added JavaDocs comments in AreaPrisma.java and Prisma.java

--- src/actividad3/AreaPrisma.java
+++ src/actividad3/AreaPrisma.java
@@ -4,24 +4,35 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
+/**
+ * Clase que genera objetos Prismas y los ordena de mayor a menor area
+ * 
+ * @author Modesto
+ *
+ */
 public class AreaPrisma {
 
+	/**
+	 * @param args Main AreaPrisma, genera numeroPrismas de objetos Prismas con
+	 *             valores aleatorios y calcula su area, y finalmente devuelve una
+	 *             lista ordenada de mayor a menor
+	 */
 	public static void main(String[] args) {
 		int numeroPrismas = 3;
 		ArrayList<Prisma> lista = new ArrayList<>(numeroPrismas);
 		Prisma prisma;
-		
-		for (int i = 0 ; i < numeroPrismas ; i++) {
+
+		for (int i = 0; i < numeroPrismas; i++) {
 			prisma = new Prisma();
 			int base = (int) (Math.random() * 30);
 			int altura = (int) (Math.random() * 20);
-			((Prisma) prisma).setNombre("Prisma"+ (lista.size()+1));
+			((Prisma) prisma).setNombre("Prisma" + (lista.size() + 1));
 			((Prisma) prisma).setBaseTriangulo(base);
 			((Prisma) prisma).setAlturaPrisma(altura);
-			((Prisma) prisma).setAreaPrisma(Prisma.areaTotal(base,altura));
+			((Prisma) prisma).setAreaPrisma(Prisma.areaTotal(base, altura));
 			lista.add(prisma);
 		}
-		
+
 		for (Prisma item : lista) {
 			System.out.println("Se ha creado " + item.nombre);
 			System.out.println("Base: " + item.baseTriangulo);
@@ -29,14 +40,14 @@ public class AreaPrisma {
 			System.out.println("Area Total: " + item.areaPrisma);
 			System.out.println("----------------------");
 		}
-		
+
 		Collections.sort(lista, new Comparator<Prisma>() {
 			@Override
 			public int compare(Prisma p1, Prisma p2) {
 				return new Float(p2.getAreaPrisma()).compareTo(new Float(p1.getAreaPrisma()));
 			}
 		});
-		
+
 		System.out.println("");
 		System.out.println("Ordenado de Mayor a menor");
 		for (Prisma item : lista) {
--- src/actividad3/Prisma.java
+++ src/actividad3/Prisma.java
@@ -1,64 +1,152 @@
 package actividad3;
 
+// TODO: Auto-generated Javadoc
+/**
+ * Clase que contiene el objeto Prisma.
+ *
+ * @author Modesto
+ */
 public class Prisma {
 
+	/** base triangulo. */
 	int baseTriangulo;
+
+	/** altura prisma. */
 	int alturaPrisma;
-	String nombre;	
+
+	/** nombre. */
+	String nombre;
+
+	/** area prisma. */
 	float areaPrisma;
-	
+
+	/**
+	 * Devuelve base triangulo.
+	 *
+	 * @return base triangulo
+	 */
 	public int getBaseTriangulo() {
 		return baseTriangulo;
 	}
 
+	/**
+	 * Introduce base triangulo.
+	 *
+	 * @param baseTriangulo es la nueva base triangulo
+	 */
 	public void setBaseTriangulo(int baseTriangulo) {
 		this.baseTriangulo = baseTriangulo;
 	}
 
+	/**
+	 * Devuelve altura prisma.
+	 *
+	 * @return altura prisma
+	 */
 	public int getAlturaPrisma() {
 		return alturaPrisma;
 	}
 
+	/**
+	 * Introduce altura prisma.
+	 *
+	 * @param alturaPrisma es la nueva altura prisma
+	 */
 	public void setAlturaPrisma(int alturaPrisma) {
 		this.alturaPrisma = alturaPrisma;
 	}
-	
-	public static float alturaTriangulo (float baseTriangulo) {
-		return (float) ((Math.sqrt(3)/2) * baseTriangulo);
+
+	/**
+	 * Altura triangulo.
+	 *
+	 * @param baseTriangulo es la base triangulo
+	 * @return raiz cuadrada de 3 partido de dos por la base del triangulo, lo cual
+	 *         es la altura
+	 */
+	public static float alturaTriangulo(float baseTriangulo) {
+		return (float) ((Math.sqrt(3) / 2) * baseTriangulo);
 	}
-	
-	public static float areaBase (float baseTriangulo, float alturaTriangulo) {
-		return (baseTriangulo * alturaTriangulo/2);
+
+	/**
+	 * Area base.
+	 *
+	 * @param baseTriangulo   es la base triangulo
+	 * @param alturaTriangulo es la altura triangulo
+	 * @return base de triangulo por la mitad de la altura del mismo
+	 */
+	public static float areaBase(float baseTriangulo, float alturaTriangulo) {
+		return (baseTriangulo * alturaTriangulo / 2);
 	}
-	
-	public static float perimetro (float baseTriangulo) {
+
+	/**
+	 * Perimetro.
+	 *
+	 * @param baseTriangulo es la base triangulo
+	 * @return tres (tres lados) por la base del triangulo
+	 */
+	public static float perimetro(float baseTriangulo) {
 		return (3 * baseTriangulo);
 	}
-	
-	public static float areaLateral (float perimetro, float alturaPrisma) {
+
+	/**
+	 * Area lateral.
+	 *
+	 * @param perimetro    es el perimetro
+	 * @param alturaPrisma es la altura prisma
+	 * @return perimetro por altura del prisma
+	 */
+	public static float areaLateral(float perimetro, float alturaPrisma) {
 		return (perimetro * alturaPrisma);
 	}
-	
+
 //	public float areaTotal (float areaBase, float areaLateral) {
 //		return (2 * areaBase + areaLateral);
 //	}
-	
-	public static float areaTotal (float baseTriangulo, float alturaPrisma) {
-		return (2 * areaBase(baseTriangulo, alturaTriangulo(baseTriangulo)) + areaLateral(perimetro(baseTriangulo), alturaPrisma));
+
+	/**
+	 * Area total.
+	 *
+	 * @param baseTriangulo es la base triangulo
+	 * @param alturaPrisma  es la altura prisma
+	 * @return 2 por la suma del area de la base y el area lateral
+	 */
+	public static float areaTotal(float baseTriangulo, float alturaPrisma) {
+		return (2 * areaBase(baseTriangulo, alturaTriangulo(baseTriangulo))
+				+ areaLateral(perimetro(baseTriangulo), alturaPrisma));
 	}
 
+	/**
+	 * Devuelve el nombre.
+	 *
+	 * @return nombre
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Introduce nombre.
+	 *
+	 * @param nombre es el nuevo nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Devuelve area prisma.
+	 *
+	 * @return area prisma
+	 */
 	public float getAreaPrisma() {
 		return areaPrisma;
 	}
 
+	/**
+	 * Introduce area prisma.
+	 *
+	 * @param areaPrisma es el nuevo area prisma
+	 */
 	public void setAreaPrisma(float areaPrisma) {
 		this.areaPrisma = areaPrisma;
 	}
